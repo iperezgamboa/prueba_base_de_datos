be23c44013331e6db8cc9986b6a148d4cc635de5
Ignacio Pèrez Gamboa <i.perezgamboa@gmail.com>
2020-09-04 11:34:10 -0400

    odeloconceptual y sql esquema

---
+++ prueba.sql
@@ -0,0 +1,31 @@
+-- Parte 1: 
+
+CREATE DATABASE prueba;
+
+c\ prueba  -- para conectar a facturas as user iperez.luego comienzo a crear las tablas. 
+
+
+
+
+
+-- Parte 2: Se debe crear el modelo en la base de datos, 
+-- en una base de datos llamada prueba , e insertar los 
+-- 5 clientes, 8 productos, 3 categorias,
+-- 10 facturas: 2 para el cliente 1, con 2 y 3 productos
+--              3 para el cliente 2, con 3, 2 y 3 productos
+--              1 para el cliente 3, con 1 producto
+--              4 para el cliente 4, con 2,3, 4 y 1 producto
+
+
+
+
+-- Parte 3: Consultas 
+
+
+-- ¿Que cliente realizó la compra más cara?
+
+
+-- ¿Que cliente pagó sobre 100 de monto?
+
+
+-- ¿Cuantos clientes han comprado el producto 6.
